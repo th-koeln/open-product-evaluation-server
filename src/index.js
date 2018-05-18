ebb8596e41b81469abe5cc1a29eeb6d99ea82dbb
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-05-18 15:08:20 +0200

    initial setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const {GraphQLServer} = require('graphql-yoga')
+const {express: middleware} = require('graphql-voyager/middleware')
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers: {}
+})
+
+server.express.use('/voyager', middleware({endpointUrl: '/'}))
+
+server.start({port: 3000}, () => console.log('Server is running on http://localhost:4000'))

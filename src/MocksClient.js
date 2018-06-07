e76abddfa6816264df506dceb45ac42a614cda47
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-06-07 17:25:46 +0200

    updated schema and split it into client and admin. Started mocking for the client side.

---
+++ src/MocksClient.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Dennis Dubbert on 07.06.18.
+ */
+const survey1 = {
+    _id : 1,
+    creationDate : "20.01.2018",
+    lastUpdate : "20.01.2018",
+    creator : user1,
+    title : "Eine sehr tolle Umfrage",
+    description : "Die Umfrage ist sehr toll. Sie ist so toll, dass sie einfach toll ist!",
+    public : true,
+    types : ["Like", "LikeDislike", "Choice", "Regulator"],
+    questions : [],
+    votes : [],
+    images : []
+}
+
+const user1 = {
+    firstname : "Max",
+    lastname : "Mustermann"
+}
+
+const user2 = {
+    firstname : "Peter",
+    lastname : "Lustig"
+}
+
+const likeQuestion = {
+
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ const {GraphQLServer} = require('graphql-yoga')
 const {express: middleware} = require('graphql-voyager/middleware')
 
 const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
+    typeDefs : './src/schemaClient.graphql',
+    resolvers : {},
+    mocks : true
+})
+
+const server2 = new GraphQLServer({
+    typeDefs: './src/schemaAdmin.graphql',
     resolvers: {},
+    mocks : true
 })
 
 server.express.use('/voyager', middleware({endpointUrl: '/'}))
+server2.express.use('/voyager', middleware({endpointUrl: '/'}))
 
 server.start({port: 3000}, () => console.log('Server is running on http://localhost:3000'))
+server2.start({port: 4000}, () => console.log('Server is running on http://localhost:4000'))

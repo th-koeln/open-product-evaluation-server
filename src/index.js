2267f1be4c233ac93ecb166b3ca48ab5fcd8ed9a
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-06-09 14:26:33 +0200

    Changed controller to context. Changed both schemas to represent the newly discussed workflow of the API. Changed the datamodel file to contain every type of the schemas.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,54 @@
-const {GraphQLServer} = require('graphql-yoga')
-const {express: middleware} = require('graphql-voyager/middleware')
+const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLScalarType } = require('graphql')
+const { express: middleware } = require('graphql-voyager/middleware')
+const { makeExecutableSchema, addMockFunctionsToSchema } = require('graphql-tools')
+const {GraphQLDateTime} = require('graphql-iso-date')
+
+const resolvers = {
+    Answer: {
+        __resolveType(obj, context, info) {
+            if(obj.liked) {
+                if(obj.question.type === 'LikeAnswer') return 'LikeAnswer'
+                else return 'LikeDislikeAnswer'
+            }
+            else if(obj.choiceCode) return 'ChoiceAnswer'
+            else if(obj.rating) return 'RegulatorAnswer'
+            else if(obj.rankedCodes) return 'RankingAnswer'
+            else if(obj.favoriteCode) return 'FavoriteAnswer'
+            else return null
+        }
+    },
+    Question: {
+        __resolveType(obj, context, info) {
+            switch (obj.type) {
+                case 'LIKE': return 'LikeQuestion'
+                case 'LIKEDISLIKE': return 'LikeDislikeQuestion'
+                case 'CHOICE': return 'ChoiceQuestion'
+                case 'REGULATOR': return 'RegulatorQuestion'
+                case 'RANKING': return 'RankingQuestion'
+                case 'FAVORITE': return 'FavoriteQuestion'
+                default : throw new Error('Unkown Question')
+            }
+        }
+    },
+    DateTime: GraphQLDateTime
+}
+
 
 const server = new GraphQLServer({
     typeDefs : './src/schemaClient.graphql',
-    resolvers : {},
-    mocks : true
+    resolvers,
+    mocks : {
+        DateTime: () => new Date()
+    }
 })
 
 const server2 = new GraphQLServer({
     typeDefs: './src/schemaAdmin.graphql',
-    resolvers: {},
-    mocks : true
+    resolvers,
+    mocks : {
+        DateTime: () => new Date()
+    }
 })
 
 server.express.use('/voyager', middleware({endpointUrl: '/'}))

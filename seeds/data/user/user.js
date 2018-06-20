24ed5f43340c27d0dc98fdd29d088df7bdbf9b4f
Sascha Lemke <sascha-lemke@gmx.de>
2018-06-20 02:51:17 +0200

    merged seeds

--- seeds/data/user/user.js
+++ seeds/data/user/user.js
@@ -7,6 +7,7 @@ const user = [
     lastUpdate: new Date(),
     firstname: 'John',
     lastname: 'Doe',
+    password: '7c4a8d09ca3762af61e59520943dc26494f8941b',
     email: 'john@doe.com',
   },
   {
@@ -15,6 +16,7 @@ const user = [
     lastUpdate: new Date(),
     firstname: 'Jane',
     lastname: 'Doe',
+    password: 'dd5fef9c1c1da1394d6d34b248c51be2ad740840',
     email: 'Jane@doe.com',
   },
   {
@@ -23,6 +25,7 @@ const user = [
     lastUpdate: new Date(),
     firstname: 'Jake',
     lastname: 'Doe',
+    password: 'fe605d3cac6d5698bd85e76ebfbdee18763519c7',
     email: 'Jake@doe.com',
   },
 ]

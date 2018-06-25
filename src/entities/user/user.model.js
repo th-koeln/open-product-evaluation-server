3addf36d926ef10403b9492be9517175241b2b41
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-06-26 00:07:12 +0200

    Implemented user.model and user.resolver. adjusted source for db-name and jwt-secret to be in .env.

--- src/entities/user/user.model.js
+++ src/entities/user/user.model.js
@@ -1,20 +1,46 @@
-// const userSchema = require('./user.schema')
+const userSchema = require('./user.schema')
 
-module.exports = db => /* {
+module.exports = (db) => {
   const User = db.model('user', userSchema, 'user')
 
-  return */ Object.freeze({
-    get: async (find, limit, offset, sort) => {
+  const isEmailFree = async email => await User.count({ email }) === 0
 
+  return Object.freeze({
+    get: async (find, limit, offset, sort) => {
+      try {
+        const users = await User.find(find).limit(limit).skip(offset).sort(sort)
+        if (users.length === 0) throw new Error('User not found.')
+        return users.map(user => user.toClient())
+      } catch (e) {
+        throw e
+      }
     },
     insert: async (object) => {
-
+      try {
+        if (await isEmailFree(object.email)) return (await new User(object).save()).toClient()
+        throw new Error('Email already in use. Could not create user.')
+      } catch (e) {
+        throw e
+      }
     },
     update: async (id, data) => {
-
+      try {
+        if (Object.prototype.hasOwnProperty.call(data, 'email') && !(await isEmailFree(data.email))) throw new Error('Email already in use. Could not update user.')
+        const updatedUser = await User.findByIdAndUpdate(id, data, { new: true })
+        if (!updatedUser) throw new Error('User not found.')
+        return updatedUser.toClient()
+      } catch (e) {
+        throw e
+      }
     },
     delete: async (id) => {
-
+      try {
+        const deletedUser = await User.findByIdAndDelete(id)
+        if (!deletedUser) throw new Error('User not found.')
+        return deletedUser.toClient()
+      } catch (e) {
+        throw e
+      }
     },
   })
-// }
+}

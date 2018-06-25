b599400c463c331d64a40199526004c0d843a5e2
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-06-25 22:35:25 +0200

    Added authUtils to encode and decode jwts. Added authMiddleware to decode Authorization-Header and put decoded auth-object into the request / context.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const { express: middleware } = require('graphql-voyager/middleware')
 const { fileLoader, mergeTypes, mergeResolvers } = require('merge-graphql-schemas')
 const path = require('path')
 const { setup } = require('./utils/dbLoader')
+const authMiddleware = require('./utils/authMiddleware')
 
 setup('openproductevaluation').then((db) => {
   const schemaList = fileLoader(path.join(__dirname, './entities/**/*.graphql'))
@@ -27,6 +28,7 @@ setup('openproductevaluation').then((db) => {
     }),
   })
 
+  server.express.use(authMiddleware)
   server.express.use('/voyager', middleware({ endpointUrl: '/' }))
 
   server.start({ port: config.app.port }, () => console.log(`Server is running on http://localhost:${config.app.port}`))
--- src/utils/authMiddleware.js
+++ src/utils/authMiddleware.js
@@ -1,4 +1,21 @@
 
+
+const authUtils = require('./authUtils')
+
 module.exports = (req, res, next) => {
-  // TODO Auth Magic
+  try {
+    const auth = req.get('Authorization')
+    if (auth) {
+      const authObject = authUtils.decode(auth)
+      req.auth = { [authObject.type]: authObject }
+    }
+    next()
+  } catch (e) {
+    res.send({
+      data: null,
+      errors: [
+        { message: e.message },
+      ],
+    })
+  }
 }

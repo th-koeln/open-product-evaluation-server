769388ba8dbdf6f0f876113d0345fd8e34603247
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-06-27 18:08:37 +0200

    reworked dbLoader to singleton. Removed db object from graphql context.

--- src/index.js
+++ src/index.js
@@ -8,27 +8,23 @@ const { GraphQLServer } = require('graphql-yoga')
 const { express: middleware } = require('graphql-voyager/middleware')
 const { fileLoader, mergeTypes, mergeResolvers } = require('merge-graphql-schemas')
 const path = require('path')
-const { setup } = require('./utils/dbLoader')
 const authMiddleware = require('./utils/authMiddleware')
 
-setup(config.db.name).then((db) => {
-  const schemaList = fileLoader(path.join(__dirname, './entities/**/*.graphql'))
-  const resolverList = fileLoader(path.join(__dirname, './entities/**/*.resolvers.js'))
+const schemaList = fileLoader(path.join(__dirname, './entities/**/*.graphql'))
+const resolverList = fileLoader(path.join(__dirname, './entities/**/*.resolvers.js'))
 
-  const server = new GraphQLServer({
-    typeDefs: mergeTypes(schemaList, { all: true }),
-    resolvers: mergeResolvers(resolverList, { all: true }),
-    /* mocks: {
-      DateTime: () => new Date(),
-    }, */
-    context: req => ({
-      ...req,
-      db,
-    }),
-  })
-
-  server.express.use(authMiddleware)
-  server.express.use('/voyager', middleware({ endpointUrl: '/' }))
-
-  server.start({ port: config.app.port }, () => console.log(`Server is running on http://localhost:${config.app.port}`))
+const server = new GraphQLServer({
+  typeDefs: mergeTypes(schemaList, { all: true }),
+  resolvers: mergeResolvers(resolverList, { all: true }),
+  /* mocks: {
+    DateTime: () => new Date(),
+  }, */
+  context: req => ({
+    ...req,
+  }),
 })
+
+server.express.use(authMiddleware)
+server.express.use('/voyager', middleware({ endpointUrl: '/' }))
+
+server.start({ port: config.app.port }, () => console.log(`Server is running on http://localhost:${config.app.port}`))

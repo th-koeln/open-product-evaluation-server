41ed09c71e7fb3060537cfb91f161adbe2805b2a
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-06-27 18:49:10 +0200

    added toClient hooks to user schema and adjusted user model. Adjusted user model to use new dbLoader. Adjusted user resolver to use new user model.

--- src/entities/user/user.model.js
+++ src/entities/user/user.model.js
@@ -1,7 +1,11 @@
 const userSchema = require('./user.schema')
+const dbLoader = require('../../utils/dbLoader')
 
-module.exports = (db) => {
-  const User = db.model('user', userSchema, 'user')
+module.exports = () => {
+  let User
+  (async () => {
+    User = (await dbLoader.getDB()).model('user', userSchema, 'user')
+  })()
 
   const isEmailFree = async email => await User.count({ email }) === 0
 
@@ -10,14 +14,14 @@ module.exports = (db) => {
       try {
         const users = await User.find(find).limit(limit).skip(offset).sort(sort)
         if (users.length === 0) throw new Error('User not found.')
-        return users.map(user => user.toClient())
+        return users
       } catch (e) {
         throw e
       }
     },
     insert: async (object) => {
       try {
-        if (await isEmailFree(object.email)) return (await new User(object).save()).toClient()
+        if (await isEmailFree(object.email)) return (await new User(object).save())
         throw new Error('Email already in use. Could not create user.')
       } catch (e) {
         throw e
@@ -28,7 +32,7 @@ module.exports = (db) => {
         if (Object.prototype.hasOwnProperty.call(data, 'email') && !(await isEmailFree(data.email))) throw new Error('Email already in use. Could not update user.')
         const updatedUser = await User.findByIdAndUpdate(id, data, { new: true })
         if (!updatedUser) throw new Error('User not found.')
-        return updatedUser.toClient()
+        return updatedUser
       } catch (e) {
         throw e
       }
@@ -37,7 +41,7 @@ module.exports = (db) => {
       try {
         const deletedUser = await User.findByIdAndDelete(id)
         if (!deletedUser) throw new Error('User not found.')
-        return deletedUser.toClient()
+        return deletedUser
       } catch (e) {
         throw e
       }

86bc92c340de36aa67e0ad39f60b3fcebdc3ada6
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-06-28 20:34:02 +0200

    added prototype of an idStore, which handles the hashing and dehashing of mongoDB ids.

---
+++ src/utils/idStore.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Dennis Dubbert on 27.06.18.
+ */
+
+'use strict'
+
+const getMatchingId = hash => hash
+const createHashFromId = id => id
+
+module.exports = {
+  getMatchingId,
+  createHashFromId,
+}

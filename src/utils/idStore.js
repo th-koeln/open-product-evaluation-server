9fb70b49e9dde408813105b80ad15829d096678f
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-06-29 12:27:50 +0200

    small bugfix (ids where not recognized by idStore).

--- src/utils/idStore.js
+++ src/utils/idStore.js
@@ -2,26 +2,25 @@
  * Created by Dennis Dubbert on 27.06.18.
  */
 const crypto = require('crypto')
-
 const secret = crypto.randomBytes(32)
-const iv = crypto.randomBytes(16)
+const iv = crypto.randomBytes(16);
 
-const getMatchingId = (hash) => {
-  const encryptedId = Buffer.from(hash, 'hex')
-  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secret), iv)
-  let decrypted = decipher.update(encryptedId)
+const getMatchingId = hash => {
+  const encryptedId = Buffer.from(hash, 'hex');
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secret), iv);
+  let decrypted = decipher.update(encryptedId);
 
-  decrypted = Buffer.concat([decrypted, decipher.final()])
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
 
-  return decrypted.toString()
+  return decrypted.toString();
 }
 
-const createHashFromId = (id) => {
-  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(secret), iv)
-  let crypted = cipher.update(id)
-  crypted = Buffer.concat([crypted, cipher.final()])
+const createHashFromId = id =>{
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(secret), iv);
+  let crypted = cipher.update(`${id}`);
+  crypted = Buffer.concat([crypted, cipher.final()]);
 
-  return crypted.toString('hex')
+  return crypted.toString('hex');
 }
 
 module.exports = {

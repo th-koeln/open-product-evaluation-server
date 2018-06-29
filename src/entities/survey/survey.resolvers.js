26040d4554a1f8c62e693e68bc74d55ab901a991
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-06-29 22:16:36 +0200

    added resolver for attribute : types

--- src/entities/survey/survey.resolvers.js
+++ src/entities/survey/survey.resolvers.js
@@ -1,7 +1,9 @@
 const surveyModel = require('./survey.model')()
 const userModel = require('../user/user.model')()
+const questionModel = require('../question/question.model')()
 const { isUser, userIdIsMatching } = require('../../utils/authUtils')
 const idStore = require('../../utils/idStore')
+const _ = require('underscore')
 
 module.exports = {
   Mutation: {
@@ -27,6 +29,14 @@ module.exports = {
       const { auth } = context.request
       if (!userIdIsMatching(auth, idStore.createHashFromId(parent.creator))) { throw new Error('Not authorized or no permissions.') }
       return (await userModel.get({ _id: parent.creator }))[0]
+    } catch (e) {
+      throw e
+    }
+  },
+  types: async (parent, args, context, info) => {
+    try {
+      const questions = await questionModel.get({ survey: parent.id })
+      return (questions.length === 0) ? null : _.uniq((questions).map(question => question.type))
     } catch (e) {
       throw e
     }

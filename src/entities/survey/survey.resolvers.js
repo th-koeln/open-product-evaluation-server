196c4e8825b7357554b0057bf60407b2b2f56924
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-06-29 17:26:47 +0200

    added createSurvey resolver

--- src/entities/survey/survey.resolvers.js
+++ src/entities/survey/survey.resolvers.js
@@ -1,3 +1,21 @@
-module.exports = {
+const surveyModel = require('./survey.model')()
+const { isUser } = require('../../utils/authUtils')
+const idStore = require('../../utils/idStore')
 
+module.exports = {
+  Mutation: {
+    createSurvey: async (parent, args, context, info) => {
+      try {
+        const { auth } = context.request
+        if (!isUser(auth)) { throw new Error('Not authorized or no permissions.') }
+        const data = { ...args.data, creator: idStore.getMatchingId(auth.user.id) }
+        const survey = await surveyModel.insert(data)
+        return {
+          survey,
+        }
+      } catch (e) {
+        throw e
+      }
+    },
+  },
 }

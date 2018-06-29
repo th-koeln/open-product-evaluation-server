aae9f7693d8033f12677feb84ceaa1a9411bc604
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-06-29 11:52:26 +0200

    Enhanced idStore to encrypt and decrypt incomming values.

--- src/utils/idStore.js
+++ src/utils/idStore.js
@@ -1,9 +1,28 @@
 /**
  * Created by Dennis Dubbert on 27.06.18.
  */
+const crypto = require('crypto')
 
-const getMatchingId = hash => hash
-const createHashFromId = id => id
+const secret = crypto.randomBytes(32)
+const iv = crypto.randomBytes(16)
+
+const getMatchingId = (hash) => {
+  const encryptedId = Buffer.from(hash, 'hex')
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secret), iv)
+  let decrypted = decipher.update(encryptedId)
+
+  decrypted = Buffer.concat([decrypted, decipher.final()])
+
+  return decrypted.toString()
+}
+
+const createHashFromId = (id) => {
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(secret), iv)
+  let crypted = cipher.update(id)
+  crypted = Buffer.concat([crypted, cipher.final()])
+
+  return crypted.toString('hex')
+}
 
 module.exports = {
   getMatchingId,

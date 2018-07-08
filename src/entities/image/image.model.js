fd0ea269ef60e16fd01d65fa1c0c75f100a90788
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-07-08 11:49:34 +0200

    implemented get of model

--- src/entities/image/image.model.js
+++ src/entities/image/image.model.js
@@ -1,18 +1,24 @@
-// const imageSchema = require('./image.schema')
+const imageSchema = require('./image.schema')
+const dbLoader = require('../../utils/dbLoader')
 
-// TODO: require dbLoader / getDB and create model
-module.exports = () =>
-  Object.freeze({
+module.exports = () => {
+  const Image = dbLoader.getDB().model('image', imageSchema, 'image')
+
+  return Object.freeze({
     get: async (find, limit, offset, sort) => {
-
+      try {
+        const images = await Image.find(find).limit(limit).skip(offset).sort(sort)
+        if (images.length === 0) throw new Error('Image not found.')
+        return images
+      } catch (e) {
+        throw e
+      }
     },
     insert: async (object) => {
-
     },
-    update: async (id, data) => {
-
+    update: async (where, data) => {
     },
-    delete: async (id) => {
-
+    delete: async (where) => {
     },
   })
+}

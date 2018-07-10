88b19954277d995bc4e9147d4589b5886235ccdf
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-07-10 23:57:50 +0200

    changed structure of model to forward declaring

--- src/entities/question/question.model.js
+++ src/entities/question/question.model.js
@@ -1,111 +1,113 @@
+const questionModel = {}
+module.exports = questionModel
+
 const questionSchema = require('./question.schema')
 const dbLoader = require('../../utils/dbLoader')
-const imageModel = require('../image/image.model')()
-const surveyModel = require('../survey/survey.model')()
+const imageModel = require('../image/image.model')
+const surveyModel = require('../survey/survey.model')
 const _ = require('underscore')
 
-module.exports = () => {
-  const Question = dbLoader.getDB().model('question', questionSchema, 'question')
+const Question = dbLoader.getDB().model('question', questionSchema, 'question')
 
-  const getQuestionsImages = (questions, key) => questions.reduce((acc, question) => {
-    let questionImages = []
-    if (Object.prototype.hasOwnProperty.call(question.toObject(), key)
-      && question.toObject()[key] !== null) {
-      questionImages = question.toObject()[key].reduce((accOldKey, object) =>
-        ((Object.prototype.hasOwnProperty.call(object, 'image')) ? [...accOldKey, object.image] : accOldKey), [])
-    }
-
-    const images = [...acc, ...questionImages]
-    return images
-  }, [])
-
-  const checkForAndDeleteRemovedImages = async (oldQuestions, updatedQuestions, key) => {
-    const oldQuestionsImages = getQuestionsImages(oldQuestions, key)
-    const updatedQuestionsImages = getQuestionsImages(updatedQuestions, key)
-    const removedImages = _.without(oldQuestionsImages, ...updatedQuestionsImages)
-
-    if (removedImages.length > 0) await imageModel.delete({ _id: { $in: removedImages } })
-    //  TODO: Check amount of deleted Images and retry those still there
+const getQuestionsImages = (questions, key) => questions.reduce((acc, question) => {
+  let questionImages = []
+  if (Object.prototype.hasOwnProperty.call(question.toObject(), key)
+    && question.toObject()[key] !== null) {
+    questionImages = question.toObject()[key].reduce((accOldKey, object) =>
+      ((Object.prototype.hasOwnProperty.call(object, 'image')) ? [...accOldKey, object.image] : accOldKey), [])
   }
 
-  return Object.freeze({
-    get: async (find, limit, offset, sort) => {
+  const images = [...acc, ...questionImages]
+  return images
+}, [])
+
+const checkForAndDeleteRemovedImages = async (oldQuestions, updatedQuestions, key) => {
+  const oldQuestionsImages = getQuestionsImages(oldQuestions, key)
+  const updatedQuestionsImages = getQuestionsImages(updatedQuestions, key)
+  const removedImages = _.without(oldQuestionsImages, ...updatedQuestionsImages)
+
+  if (removedImages.length > 0) await imageModel.delete({ _id: { $in: removedImages } })
+  //  TODO: Check amount of deleted Images and retry those still there
+}
+
+questionModel.get = async (find, limit, offset, sort) => {
+  try {
+    const questions = await Question.find(find).limit(limit).skip(offset).sort(sort)
+    if (questions.length === 0) throw new Error('No Question found.')
+    return questions
+  } catch (e) {
+    throw e
+  }
+}
+
+questionModel.insert = async (object) => {
+  try {
+    const question = await new Question(object).save()
+    try {
+      await surveyModel.update({ _id: question.survey }, { $push: { questions: question.id } })
+    } catch (e) {
+      console.log(e)
+    }
+    return question
+  } catch (e) {
+    throw e
+  }
+}
+
+questionModel.update = async (where, data) => {
+  try {
+    let oldQuestions
+    if (data.items || data.labels || data.choices) oldQuestions = await Question.find(where)
+    const result = await Question.updateMany(where, data)
+    if (result.nMatched === 0) throw new Error('No Question found.')
+    if (result.nModified === 0) throw new Error('Question update failed.')
+    const updatedQuestions = await Question.find(where)
+
+    if (oldQuestions) {
       try {
-        const questions = await Question.find(find).limit(limit).skip(offset).sort(sort)
-        if (questions.length === 0) throw new Error('No Question found.')
-        return questions
+        if (Object.prototype.hasOwnProperty.call(data, 'items')) await checkForAndDeleteRemovedImages(oldQuestions, updatedQuestions, 'items')
       } catch (e) {
-        throw e
+        console.log(e)
       }
-    },
-    insert: async (object) => {
       try {
-        const question = await new Question(object).save()
-        try {
-          await surveyModel.update({ _id: question.survey }, { $push: { questions: question.id } })
-        } catch (e) {
-          console.log(e)
-        }
-        return question
+        if (Object.prototype.hasOwnProperty.call(data, 'labels')) await checkForAndDeleteRemovedImages(oldQuestions, updatedQuestions, 'labels')
       } catch (e) {
-        throw e
+        console.log(e)
       }
-    },
-    update: async (where, data) => {
       try {
-        let oldQuestions
-        if (data.items || data.labels || data.choices) oldQuestions = await Question.find(where)
-        const result = await Question.updateMany(where, data)
-        if (result.nMatched === 0) throw new Error('No Question found.')
-        if (result.nModified === 0) throw new Error('Question update failed.')
-        const updatedQuestions = await Question.find(where)
-
-        if (oldQuestions) {
-          try {
-            if (Object.prototype.hasOwnProperty.call(data, 'items')) await checkForAndDeleteRemovedImages(oldQuestions, updatedQuestions, 'items')
-          } catch (e) {
-            console.log(e)
-          }
-          try {
-            if (Object.prototype.hasOwnProperty.call(data, 'labels')) await checkForAndDeleteRemovedImages(oldQuestions, updatedQuestions, 'labels')
-          } catch (e) {
-            console.log(e)
-          }
-          try {
-            if (Object.prototype.hasOwnProperty.call(data, 'choices')) await checkForAndDeleteRemovedImages(oldQuestions, updatedQuestions, 'choices')
-          } catch (e) {
-            console.log(e)
-          }
-        }
-
-        return updatedQuestions
+        if (Object.prototype.hasOwnProperty.call(data, 'choices')) await checkForAndDeleteRemovedImages(oldQuestions, updatedQuestions, 'choices')
       } catch (e) {
-        throw e
+        console.log(e)
       }
-    },
-    delete: async (where) => {
-      try {
-        const questions = await Question.find(where)
-        const result = await Question.deleteMany(where)
-        if (result.n === 0) throw new Error('Question deletion failed.')
+    }
 
-        const itemImages = getQuestionsImages(questions, 'items')
-        const labelImages = getQuestionsImages(questions, 'labels')
-        const choiceImages = getQuestionsImages(questions, 'choices')
-        const imagesToDelete = _.uniq([...itemImages, ...labelImages, ...choiceImages])
+    return updatedQuestions
+  } catch (e) {
+    throw e
+  }
+}
 
-        try {
-          if (imagesToDelete.length > 0) await imageModel.delete({ _id: { $in: imagesToDelete } })
-          //  TODO: Check amount of deleted Images and retry those still there
-        } catch (e) {
-          console.log(e)
-        }
+questionModel.delete = async (where) => {
+  try {
+    const questions = await Question.find(where)
+    const result = await Question.deleteMany(where)
+    if (result.n === 0) throw new Error('Question deletion failed.')
 
-        //  TODO: Check amount of deleted Questions and retry those still there
-        return result
-      } catch (e) {
-        throw e
-      }
-    },
-  })
+    const itemImages = getQuestionsImages(questions, 'items')
+    const labelImages = getQuestionsImages(questions, 'labels')
+    const choiceImages = getQuestionsImages(questions, 'choices')
+    const imagesToDelete = _.uniq([...itemImages, ...labelImages, ...choiceImages])
+
+    try {
+      if (imagesToDelete.length > 0) await imageModel.delete({ _id: { $in: imagesToDelete } })
+      //  TODO: Check amount of deleted Images and retry those still there
+    } catch (e) {
+      console.log(e)
+    }
+
+    //  TODO: Check amount of deleted Questions and retry those still there
+    return result
+  } catch (e) {
+    throw e
+  }
 }

86b3c0fd53aa2b2eba543cc630ea0a9efb5e89fc
Fabian Daugs <fabian.daugs@smail.th-koeln.de>
2018-07-11 13:35:46 +0200

    changed structure of deviceModel to forward declaring

--- src/entities/device/device.model.js
+++ src/entities/device/device.model.js
@@ -1,45 +1,45 @@
+const deviceModel = {}
+
+module.exports = deviceModel
+
 const deviceSchema = require('./device.schema')
 const dbLoader = require('../../utils/dbLoader')
 
-module.exports = () => {
-  const Device = dbLoader.getDB().model('device', deviceSchema, 'device')
+const Device = dbLoader.getDB().model('device', deviceSchema, 'device')
 
-  return Object.freeze({
-    get: async (find, limit, offset, sort) => {
-      try {
-        const devices = await Device.find(find).limit(limit).skip(offset).sort(sort)
-        if (devices.length === 0) throw new Error('No devices found')
-        return devices
-      } catch (e) {
-        throw e
-      }
-    },
-    insert: async (object) => {
-      try {
-        return (await new Device(object).save())
-      } catch (e) {
-        throw e
-      }
-    },
-    update: async (where, data) => {
-      try {
-        const result = await Device.updateMany(where, data)
-        if (result.nMatched === 0) throw new Error('Device not found.')
-        if (result.nModified === 0) throw new Error('Device update failed.')
-        const updatedDevice = await Device.find(where)
-        return updatedDevice
-      } catch (e) {
-        throw e
-      }
-    },
-    delete: async (where) => {
-      try {
-        const result = await Device.deleteMany(where)
-        if (result.n === 0) throw new Error('Device deletion failed.')
-        return result
-      } catch (e) {
-        throw e
-      }
-    },
-  })
+deviceModel.get = async (find, limit, offset, sort) => {
+  try {
+    const devices = await Device.find(find).limit(limit).skip(offset).sort(sort)
+    if (devices.length === 0) throw new Error('No devices found')
+    return devices
+  } catch (e) {
+    throw e
+  }
+}
+deviceModel.insert = async (object) => {
+  try {
+    return (await new Device(object).save())
+  } catch (e) {
+    throw e
+  }
+}
+deviceModel.update = async (where, data) => {
+  try {
+    const result = await Device.updateMany(where, data)
+    if (result.nMatched === 0) throw new Error('Device not found.')
+    if (result.nModified === 0) throw new Error('Device update failed.')
+    const updatedDevice = await Device.find(where)
+    return updatedDevice
+  } catch (e) {
+    throw e
+  }
+}
+deviceModel.delete = async (where) => {
+  try {
+    const result = await Device.deleteMany(where)
+    if (result.n === 0) throw new Error('Device deletion failed.')
+    return result
+  } catch (e) {
+    throw e
+  }
 }

76c6e7b555b7df2ea4585aca1e85cc64355e597c
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-07-11 20:03:02 +0200

    implemented get method of voteModel

--- src/entities/vote/vote.model.js
+++ src/entities/vote/vote.model.js
@@ -1,10 +1,19 @@
 const voteModel = {}
 module.exports = voteModel
 
-// const voteSchema = require('./vote.schema')
+const voteSchema = require('./vote.schema')
+const dbLoader = require('../../utils/dbLoader')
+
+const Vote = dbLoader.getDB().model('vote', voteSchema, 'vote')
 
 voteModel.get = async (find, limit, offset, sort) => {
-
+  try {
+    const votes = await Vote.find(find).limit(limit).skip(offset).sort(sort)
+    if (votes.length === 0) throw new Error('No Votes found')
+    return votes
+  } catch (e) {
+    throw e
+  }
 }
 
 voteModel.insert = async (object) => {

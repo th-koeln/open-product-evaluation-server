b1431bd79fbcbf683f94955f9bb3e90c5129440c
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-07-11 22:24:16 +0200

    prepared basics for creating answers

--- src/entities/vote/vote.resolvers.js
+++ src/entities/vote/vote.resolvers.js
@@ -1,10 +1,46 @@
-const { createHashFromId } = require('../../utils/idStore')
+const deviceModel = require('../device/device.model')
+const contextModel = require('../context/context.model')
+const surveyModel = require('../survey/survey.model')
+const { getMatchingId, createHashFromId } = require('../../utils/idStore')
+const { isDevice } = require('../../utils/authUtils')
 
 const sharedResolvers = {
   question: async (parent, args, context, info) => createHashFromId(parent.question),
 }
 
+const getDeviceDependencies = async (auth) => {
+  if (!isDevice(auth)) throw new Error('Not authorized or no permissions.')
+  const [device] = await deviceModel.get({ _id: getMatchingId(auth.device.id) })
+
+  if (!(Object.prototype.hasOwnProperty.call(device.toObject(), 'context')
+    && device.context !== null && device.context !== '')) throw new Error('This Device is not connected to a Context.')
+  const [context] = await contextModel.get({ _id: device.context })
+
+  if (!(Object.prototype.hasOwnProperty.call(context.toObject(), 'activeSurvey')
+    && context.activeSurvey !== null && context.activeSurvey !== '')) throw new Error('The Context of this Device is not connected to a Survey.')
+  const [survey] = await surveyModel.get({ _id: context.activeSurvey })
+
+  return {
+    device,
+    context,
+    survey,
+  }
+}
+
 module.exports = {
+  Mutation: {
+    createAnswer: async (parent, { data }, { request }, info) => {
+      try {
+        const { auth } = request
+        const deviceDependencies = getDeviceDependencies(auth)
+        console.log(deviceDependencies)
+        /** Call answerStore to store the Answer * */
+        /** If answerStore returns vote, persist it in DB * */
+      } catch (e) {
+        throw e
+      }
+    },
+  },
   Vote: {
     id: async (parent, args, context, info) => createHashFromId(parent.id),
     context: async (parent, args, context, info) => ((Object.prototype.hasOwnProperty.call(parent.toObject(), 'context')
---
+++ src/utils/answerStore.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Dennis Dubbert on 11.07.18.
+ */
+
+const createAnswer = async (deviceDependencies, answerInput) => {
+  console.log(deviceDependencies)
+  console.log(answerInput)
+  return 'kot'
+}
+
+module.exports = {
+  createAnswer,
+}

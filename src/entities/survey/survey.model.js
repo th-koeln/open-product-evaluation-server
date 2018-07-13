1d36397dcc599fc44a186e905b3bf09645d7f818
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-07-13 16:29:44 +0200

    enhanced delete method of survey model to also clear survey from answerStore cache

--- src/entities/survey/survey.model.js
+++ src/entities/survey/survey.model.js
@@ -6,6 +6,8 @@ const dbLoader = require('../../utils/dbLoader')
 const questionModel = require('../question/question.model')
 const contextModel = require('../context/context.model')
 const imageModel = require('../image/image.model')
+const _ = require('underscore')
+const { removeSurveyFromCache } = require('../../utils/answerStore')
 
 const Survey = dbLoader.getDB().model('survey', surveySchema, 'survey')
 
@@ -45,36 +47,43 @@ surveyModel.delete = async (where) => {
     if (surveys.length === 0) throw new Error('No Survey found.')
     const result = await Survey.deleteMany(where)
     if (result.n === 0) throw new Error('Survey deletion failed.')
-    const surveyIds = surveys.reduce((acc, survey) => ([...acc, survey._id]), [])
-    /** Delete Questions and all sub-documents * */
-    try {
-      await questionModel.delete({ survey: { $in: surveyIds } })
-    } catch (e) {
-      // TODO:
-      // ggf. Modul erstellen, welches fehlgeschlagene DB-Zugriffe
-      // in bestimmten abständen wiederholt
-      // (nur für welche, die nicht ausschlaggebend für erfolg der query sind)
-      console.log(e)
-    }
-    /** Delete Images and all sub-documents * */
-    try {
-      await imageModel.delete({ survey: { $in: surveyIds } })
-    } catch (e) {
-      // TODO:
-      // ggf. Modul erstellen, welches fehlgeschlagene DB-Zugriffe
-      // in bestimmten abständen wiederholt
-      // (nur für welche, die nicht ausschlaggebend für erfolg der query sind)
-      console.log(e)
-    }
-    /** Update all contexts referencing this survey * */
-    try {
-      await contextModel.update({ activeSurvey: { $in: surveyIds } }, { $unset: { activeSurvey: '', activeQuestion: '' } })
-    } catch (e) {
-      // TODO:
-      // ggf. Modul erstellen, welches fehlgeschlagene DB-Zugriffe
-      // in bestimmten abständen wiederholt
-      // (nur für welche, die nicht ausschlaggebend für erfolg der query sind)
-      console.log(e)
+    const surveyIds = surveys.reduce((acc, survey) => ([...acc, `${survey._id}`]), [])
+    const notDeletedSurveys = await Survey.find(where)
+    const notDeletedIds = notDeletedSurveys.reduce((acc, survey) => ([...acc, `${survey._id}`]), [])
+    const deletedIds = _.without(surveyIds, ...notDeletedIds)
+
+    if (deletedIds.length > 0) {
+      deletedIds.forEach(surveyId => removeSurveyFromCache(surveyId))
+      /** Delete Questions and all sub-documents * */
+      try {
+        await questionModel.delete({ survey: { $in: deletedIds } })
+      } catch (e) {
+        // TODO:
+        // ggf. Modul erstellen, welches fehlgeschlagene DB-Zugriffe
+        // in bestimmten abständen wiederholt
+        // (nur für welche, die nicht ausschlaggebend für erfolg der query sind)
+        console.log(e)
+      }
+      /** Delete Images and all sub-documents * */
+      try {
+        await imageModel.delete({ survey: { $in: deletedIds } })
+      } catch (e) {
+        // TODO:
+        // ggf. Modul erstellen, welches fehlgeschlagene DB-Zugriffe
+        // in bestimmten abständen wiederholt
+        // (nur für welche, die nicht ausschlaggebend für erfolg der query sind)
+        console.log(e)
+      }
+      /** Update all contexts referencing this survey * */
+      try {
+        await contextModel.update({ activeSurvey: { $in: deletedIds } }, { $unset: { activeSurvey: '', activeQuestion: '' } })
+      } catch (e) {
+        // TODO:
+        // ggf. Modul erstellen, welches fehlgeschlagene DB-Zugriffe
+        // in bestimmten abständen wiederholt
+        // (nur für welche, die nicht ausschlaggebend für erfolg der query sind)
+        console.log(e)
+      }
     }
     return result
   } catch (e) {

7bcb90d2725a6edc8f25108ae44200bfd1486491
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-07-14 18:45:14 +0200

    changed authMiddleware to get user/device object from db

--- src/utils/authMiddleware.js
+++ src/utils/authMiddleware.js
@@ -1,20 +1,34 @@
-
-
+const userModel = require('../entities/user/user.model')
+const deviceModel = require('../entities/device/device.model')
+const { getMatchingId } = require('./idStore')
 const authUtils = require('./authUtils')
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   try {
     const auth = req.get('Authorization')
     if (auth) {
       const authObject = authUtils.decode(auth)
-      req.auth = { [authObject.type]: authObject }
+      let entity
+      switch (authObject.type) {
+        case 'user': {
+          entity = await userModel.get({ _id: getMatchingId(authObject.id) })
+          break
+        }
+        case 'device': {
+          entity = await deviceModel.get({ _id: getMatchingId(authObject.id) })
+          break
+        }
+        default: throw new Error('Unknown entity.')
+      }
+
+      req.auth = { [authObject.type]: entity }
     }
     next()
   } catch (e) {
     res.send({
       data: null,
       errors: [
-        { message: e.message },
+        { message: 'Authorization failed' },
       ],
     })
   }

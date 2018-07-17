96267172354901f6e75577ead91035b8731c0093
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-07-17 17:54:59 +0200

    fixed imageStore to not overwrite images with same name

--- src/utils/imageStore.js
+++ src/utils/imageStore.js
@@ -3,7 +3,7 @@
  */
 // TODO: this is only a prototype. Extend functionality (real hashes etc.)
 const {
-  createWriteStream, ensureDir, remove, pathExists, readdir,
+  createWriteStream, ensureDir, remove, pathExists, readdir, existsSync,
 } = require('fs-extra')
 const config = require('../../config')
 const { createHashFromId } = require('./idStore')
@@ -15,19 +15,32 @@ const isImage = (mimetype) => {
   return types[0] === 'image' && mimeList.indexOf(types[1].toLowerCase()) !== -1
 }
 
+const getAllowedFilename = (filename, mimetype, userFolder) => {
+  const mime = mimetype.split('/')[1]
+  const name = filename.replace(/\.[^/.]+$/, '')
+  let allowedFilename = `${name}.${mime}`
+  let counter = 1
+  while (existsSync(`${userFolder}/${allowedFilename}`)) {
+    allowedFilename = `${name + counter}.${mime}`
+    counter += 1
+  }
+  return allowedFilename
+}
+
 const saveImage = async (file, user) => {
   const { stream, filename, mimetype } = file
   if (!isImage(mimetype)) throw new Error('File is not an Image.')
   return new Promise((resolve, reject) => {
     const userFolder = `${config.app.imageFolder}/${createHashFromId(user)}`
+    const allowedFilename = getAllowedFilename(filename, mimetype, userFolder)
     ensureDir(userFolder).then(() => {
       stream
-        .pipe(createWriteStream(`${userFolder}/${filename}`))
+        .pipe(createWriteStream(`${userFolder}/${allowedFilename}`))
         .on('finish', () => resolve({
-          name: filename,
+          name: allowedFilename,
           type: mimetype,
           hash: 'hash',
-          url: `${config.app.rootURL}:${config.app.port}/${userFolder}/${filename}`,
+          url: `${config.app.rootURL}:${config.app.port}/${userFolder}/${allowedFilename}`,
         }))
         .on('error', () => reject(new Error('Image upload failed.')))
     })

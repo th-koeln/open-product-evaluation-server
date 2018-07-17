f1deb13f431ebfddc58ff862edeeb1008555d638
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-07-17 22:16:35 +0200

    added hash creation to imageStore and save images with hashed name

--- src/utils/imageStore.js
+++ src/utils/imageStore.js
@@ -6,6 +6,7 @@ const {
   createWriteStream, ensureDir, remove, pathExists, readdir, existsSync,
 } = require('fs-extra')
 const config = require('../../config')
+const crypto = require('crypto')
 const { createHashFromId } = require('./idStore')
 
 const mimeList = ['jpeg', 'png', 'gif', 'bmp', 'webp']
@@ -15,16 +16,17 @@ const isImage = (mimetype) => {
   return types[0] === 'image' && mimeList.indexOf(types[1].toLowerCase()) !== -1
 }
 
-const getAllowedFilename = (filename, mimetype, userFolder) => {
-  const mime = mimetype.split('/')[1]
+const getAllowedFilenameAndHash = (filename, mime, userFolder) => {
   const name = filename.replace(/\.[^/.]+$/, '')
   let allowedFilename = `${name}.${mime}`
+  let hash = crypto.createHash('md5').update(name).digest('hex')
   let counter = 1
-  while (existsSync(`${userFolder}/${allowedFilename}`)) {
+  while (existsSync(`${userFolder}/${hash}.${mime}`)) {
     allowedFilename = `${name + counter}.${mime}`
+    hash = crypto.createHash('md5').update(name + counter).digest('hex')
     counter += 1
   }
-  return allowedFilename
+  return { allowedFilename, hash }
 }
 
 const saveImage = async (file, user) => {
@@ -32,24 +34,25 @@ const saveImage = async (file, user) => {
   if (!isImage(mimetype)) throw new Error('File is not an Image.')
   return new Promise((resolve, reject) => {
     const userFolder = `${config.app.imageFolder}/${createHashFromId(user)}`
-    const allowedFilename = getAllowedFilename(filename, mimetype, userFolder)
+    const mime = mimetype.split('/')[1]
+    const { allowedFilename, hash } = getAllowedFilenameAndHash(filename, mime, userFolder)
     ensureDir(userFolder).then(() => {
       stream
-        .pipe(createWriteStream(`${userFolder}/${allowedFilename}`))
+        .pipe(createWriteStream(`${userFolder}/${hash}.${mime}`))
         .on('finish', () => resolve({
           name: allowedFilename,
-          type: mimetype,
-          hash: 'hash',
-          url: `${config.app.rootURL}:${config.app.port}/${userFolder}/${allowedFilename}`,
+          type: mime,
+          hash,
+          url: `${config.app.rootURL}:${config.app.port}/${userFolder}/${hash}.${mime}`,
         }))
         .on('error', () => reject(new Error('Image upload failed.')))
     })
   })
 }
 
-const removeImage = async (filename, user) => {
+const removeImage = async (hash, mime, user) => {
   const userFolder = `${config.app.imageFolder}/${createHashFromId(user)}`
-  const filePath = `${userFolder}/${filename}`
+  const filePath = `${userFolder}/${hash}.${mime}`
   if (await pathExists(userFolder)) {
     if (await pathExists(filePath)) await remove(filePath)
 

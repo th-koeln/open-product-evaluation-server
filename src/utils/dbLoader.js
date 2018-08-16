57f4b8466f702922fc73cfec4b7e9dfc66464194
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-08-16 12:45:01 +0200

    reworked dbLoader to create models and inject their dependencies

--- src/utils/dbLoader.js
+++ src/utils/dbLoader.js
@@ -2,6 +2,14 @@
  * Created by Dennis Dubbert on 22.06.18.
  */
 
+const contextModel = require('../entities/context/context.model')
+const deviceModel = require('../entities/device/device.model')
+const imageModel = require('../entities/image/image.model')
+const questionModel = require('../entities/question/question.model')
+const surveyModel = require('../entities/survey/survey.model')
+const userModel = require('../entities/user/user.model')
+const voteModel = require('../entities/vote/vote.model')
+
 const mongoose = require('mongoose')
 
 mongoose.Promise = Promise
@@ -10,7 +18,18 @@ const config = require('../../config')
 let db
 
 module.exports = {
-  getDB: () => db,
+  getModules: (eventEmitter) => {
+    const modules = {}
+    modules.context = contextModel(db, eventEmitter)
+    modules.device = deviceModel(db, eventEmitter)
+    modules.image = imageModel(db, eventEmitter)
+    modules.question = questionModel(db, eventEmitter)
+    modules.survey = surveyModel(db, eventEmitter)
+    modules.user = userModel(db, eventEmitter)
+    modules.vote = voteModel(db, eventEmitter)
+
+    return modules
+  },
   connectDB: async () => {
     db = await mongoose.connect(`mongodb://localhost/${config.db.name}`)
     console.log('connecting db')

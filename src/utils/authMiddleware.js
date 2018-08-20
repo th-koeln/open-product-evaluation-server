9c825f6c9760d3aa939ca38780158006dcfb131f
Dennis Dubbert <dennis.dubbert@smail.th-koeln.de>
2018-08-20 20:20:14 +0200

    reworked authMiddleware to make use of dependency injection for getting dbModels (factory function)

--- src/utils/authMiddleware.js
+++ src/utils/authMiddleware.js
@@ -1,23 +1,21 @@
-const userModel = require('../entities/user/user.model')
-const deviceModel = require('../entities/device/device.model')
 const { getMatchingId } = require('./idStore')
-const authUtils = require('./authUtils')
+const { decode } = require('./authUtils')
 
-module.exports = async (req, res, next) => {
+module.exports = models => async (req, res, next) => {
   try {
     const auth = req.get('Authorization')
     if (auth) {
-      const authObject = authUtils.decode(auth)
+      const authObject = decode(auth)
       const matchingId = getMatchingId(authObject.id)
 
       let entity
       switch (authObject.type) {
         case 'user': {
-          [entity] = await userModel.get({ _id: matchingId })
+          [entity] = await models.user.get({ _id: matchingId })
           break
         }
         case 'device': {
-          [entity] = await deviceModel.get({ _id: matchingId })
+          [entity] = await models.device.get({ _id: matchingId })
           break
         }
         default: throw new Error('Unknown entity.')
@@ -25,6 +23,7 @@ module.exports = async (req, res, next) => {
 
       req.auth = { [authObject.type]: entity }
     }
+
     next()
   } catch (e) {
     res.send({

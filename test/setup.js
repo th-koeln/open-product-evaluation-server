4b78f58a858c2584713ab00f3a1ab89e53ac0836
Fabian Daugs <fabian.daugs@smail.th-koeln.de>
2018-09-19 21:06:53 +0200

    implemented requestHelper for testing

---
+++ test/setup.js
@@ -0,0 +1,9 @@
+const { seedDatabase } = require('mongo-seeding')
+const config = require('../config')
+const chai = require('chai')
+
+chai.should()
+
+
+beforeEach(async () => seedDatabase(config.seeder))
+

0361b4389e74c7b379d2c90df4696f127fb5759f
ddubbert <dennis.dubbert@smail.th-koeln.de>
2018-12-03 18:43:42 +0100

    changed types of owners to be Strings instead of ids (e.g. emails)

--- src/entities/client/client.schema.js
+++ src/entities/client/client.schema.js
@@ -5,13 +5,7 @@ const { Schema } = mongoose
 const Client = new Schema({
   name: { type: String, required: true },
   domain: { type: Schema.Types.ObjectId, get: id => ((id) ? id.toString() : null) },
-  owners: {
-    type: [Schema.Types.ObjectId],
-    get: (arr) => {
-      if (arr) { return arr.map(id => id.toString()) }
-      return []
-    },
-  },
+  owners: [String],
 }, { timestamps: { createdAt: 'creationDate', updatedAt: 'lastUpdate' } })
 
 module.exports = Client

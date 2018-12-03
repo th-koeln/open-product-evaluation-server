18e2dacb84c330c997639583be24e43146837039
ddubbert <dennis.dubbert@smail.th-koeln.de>
2018-12-03 18:05:25 +0100

    changed type of owners in schemas from id to string

--- src/entities/domain/domain.schema.js
+++ src/entities/domain/domain.schema.js
@@ -11,13 +11,7 @@ const Domain = new Schema({
   name: { type: String, required: true },
   activeQuestion: { type: Schema.Types.ObjectId, get: id => ((id) ? id.toString() : null) },
   activeSurvey: { type: Schema.Types.ObjectId, get: id => ((id) ? id.toString() : null) },
-  owners: {
-    type: [Schema.Types.ObjectId],
-    get: (arr) => {
-      if (arr) { return arr.map(id => id.toString()) }
-      return []
-    },
-  },
+  owners: [String],
   states: [State],
 }, { timestamps: { createdAt: 'creationDate', updatedAt: 'lastUpdate' } })
 

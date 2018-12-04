13399ce7a525c14014ad4fe76ca9b4db66c3ee91
ddubbert <dennis.dubbert@smail.th-koeln.de>
2018-12-04 22:18:20 +0100

    adjusted the permissionMiddleware to match the structure of #149

--- src/utils/permissionMiddleware.js
+++ src/utils/permissionMiddleware.js
@@ -2,22 +2,21 @@ const {
   rule, shield, or, allow,
 } = require('graphql-shield')
 
-const { ADMIN, USER } = require('./roles')
+const { ADMIN, USER, CLIENT } = require('./roles')
 
-const isAuthenticated = rule()(async (parent, args, { request }, info) => request.auth !== null)
+const isAuthenticated = rule()(async (parent, args, { request }) => request.auth !== null)
 
-const isAdmin = rule()(async (parent, args, { request }, info) => request.auth.role === ADMIN)
+const isAdmin = rule()(async (parent, args, { request }) => request.auth.role === ADMIN)
 
-const isUser = rule()(async (parent, args, { request }, info) => request.auth.role === USER)
+const isUser = rule()(async (parent, args, { request }) => request.auth.role === USER)
 
-// const isClient = rule()(async (parent, args, ctx, info) => ctx.auth.role === CLIENT)
+const isClient = rule()(async (parent, args, { request }) => request.auth.role === CLIENT)
 
 
 const permissions = shield({
   Query: {
     domains: isAuthenticated,
     domain: isAuthenticated,
-    state: isAuthenticated,
     clients: isAuthenticated,
     client: isAuthenticated,
     surveys: or(isAdmin, isUser),
@@ -30,27 +29,36 @@ const permissions = shield({
     createDomain: or(isAdmin, isUser),
     updateDomain: isAuthenticated,
     deleteDomain: or(isAdmin, isUser),
-    createState: isAuthenticated,
-    updateState: isAuthenticated,
-    deleteState: isAuthenticated,
+    setDomainOwner: or(isAdmin, isUser),
+    removeDomainOwner: or(isAdmin, isUser),
+    setState: isAuthenticated,
+    removeState: isAuthenticated,
     createClient: allow,
     updateClient: isAuthenticated,
     deleteClient: isAuthenticated,
-    createBonusImage: or(isAdmin, isUser),
-    updateBonusImage: or(isAdmin, isUser),
-    deleteBonusImage: or(isAdmin, isUser),
+    setClientOwner: isAuthenticated,
+    removeClientOwner: isAuthenticated,
+    createSurveyImage: or(isAdmin, isUser),
+    updateSurveyImage: or(isAdmin, isUser),
+    deleteSurveyImage: or(isAdmin, isUser),
     createQuestion: or(isAdmin, isUser),
     updateQuestion: or(isAdmin, isUser),
     deleteQuestion: or(isAdmin, isUser),
     createItem: or(isAdmin, isUser),
     updateItem: or(isAdmin, isUser),
     deleteItem: or(isAdmin, isUser),
+    setItemImage: or(isAdmin, isUser),
+    removeItemImage: or(isAdmin, isUser),
     createLabel: or(isAdmin, isUser),
     updateLabel: or(isAdmin, isUser),
     deleteLabel: or(isAdmin, isUser),
+    setLabelImage: or(isAdmin, isUser),
+    removeLabelImage: or(isAdmin, isUser),
     createChoice: or(isAdmin, isUser),
     updateChoice: or(isAdmin, isUser),
     deleteChoice: or(isAdmin, isUser),
+    setChoiceImage: or(isAdmin, isUser),
+    removeChoiceImage: or(isAdmin, isUser),
     createSurvey: or(isAdmin, isUser),
     updateSurvey: or(isAdmin, isUser),
     deleteSurvey: or(isAdmin, isUser),
@@ -58,7 +66,8 @@ const permissions = shield({
     updateUser: or(isAdmin, isUser),
     deleteUser: or(isAdmin, isUser),
     login: allow,
-    createAnswer: isAuthenticated,
+    setAnswer: isClient,
+    removeAnswer: isClient,
   },
   Domain: {
     owners: or(isAdmin, isUser),

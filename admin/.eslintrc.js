8de181c93f2ae523131fa32f52bcb12aad18d3ce
Sascha Lemke <sascha-lemke@gmx.de>
2019-02-02 19:41:30 +0100

    update vue linter rules

--- admin/.eslintrc.js
+++ admin/.eslintrc.js
@@ -36,5 +36,16 @@ module.exports = {
     ],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/max-attributes-per-line': ["error", {
+      "singleline": 1,
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": true
+      }
+    }],
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "never"
+    }]
   }
 }

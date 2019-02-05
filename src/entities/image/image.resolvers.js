07cfa3e3dcfa61bccdd6a98233d585099b1558d9
ddubbert <dennis.dubbert@smail.th-koeln.de>
2019-02-05 22:36:11 +0100

    rewrote createSurveyImage to setSurveyPreviewImage in image resolvers (now adds a previewImage to a survey and removes the old one if present)

--- src/entities/image/image.resolvers.js
+++ src/entities/image/image.resolvers.js
@@ -4,17 +4,25 @@ const { ADMIN } = require('../../utils/roles')
 
 module.exports = {
   Mutation: {
-    createSurveyImage: async (parent, { data, image }, { request, models, imageStore }) => {
-      // TODO: Check for already uploaded images and their size -> cancel if limit already reached
+    setSurveyPreviewImage: async (parent, { data, image }, { request, models, imageStore }) => {
       const { auth } = request
       const [survey] = await models.survey.get({ _id: getMatchingId(data.surveyID) })
 
       if (!(auth.role === ADMIN || auth.id === survey.creator)) { throw new Error('Not authorized or no permissions.') }
+
+      let oldImage
+      try {
+        oldImage = await models.image.get({ survey: survey.id })
+      } catch (e) { oldImage = null }
+
+      if (oldImage) {
+        await models.image.delete({ _id: oldImage.id })
+      }
+
       const upload = await imageStore.saveImage(await image, survey.creator)
 
       upload.user = survey.creator
       upload.survey = survey.id
-      if (data.tags) { upload.tags = data.tags }
 
       try {
         return { image: await models.image.insert(upload) }
@@ -23,20 +31,7 @@ module.exports = {
         throw new Error('Image upload failed. Try again later.')
       }
     },
-    updateSurveyImage: async (parent, { data, imageID }, { request, models }) => {
-      const { auth } = request
-      const matchingId = getMatchingId(imageID)
-
-      const [{ user: creatorId }] = await models.image.get({ _id: matchingId })
-      if (!(auth.role === ADMIN || auth.id === creatorId)) { throw new Error('Not authorized or no permissions.') }
-
-      const [imageData] = await models.image.update({ _id: matchingId }, data)
-
-      return {
-        image: imageData,
-      }
-    },
-    deleteSurveyImage: async (parent, { imageID }, { request, models }) => {
+    removeSurveyPreviewImage: async (parent, { imageID }, { request, models }) => {
       const { auth } = request
       const matchingId = getMatchingId(imageID)
 
@@ -50,7 +45,6 @@ module.exports = {
   },
   ImageData: {
     id: async ({ id }) => createHashFromId(id),
-    tags: async ({ tags }) => ((!tags || tags.length === 0) ? null : tags),
     url: async ({ url }) => `${config.app.rootURL}:${config.app.port}/${url}`,
   },
 }

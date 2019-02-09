c0d8faed694740868df752734dbe882dec20844a
Sascha Lemke <sascha-lemke@gmx.de>
2019-02-09 21:52:37 +0100

    merged itcss approach

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: '@import "@/scss/settings/settings.scss";',
+        data: '@import "@/scss/variables.scss";',
       },
     },
   },

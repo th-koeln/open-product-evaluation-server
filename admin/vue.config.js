47623f997979f1b6084b731117afc5d0f2d1142c
Sascha Lemke <sascha-lemke@gmx.de>
2019-02-09 18:08:06 +0100

    improved scss setup

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: '@import "@/scss/variables.scss";',
+        data: '@import "@/scss/settings/settings.scss";',
       },
     },
   },

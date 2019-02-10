896bdd6f0f1e1f8c85e94645139ac484e5255add
Sascha Lemke <sascha-lemke@gmx.de>
2019-02-10 02:58:52 +0100

    added login tests

---
+++ admin/tests/e2e/specs/account/login.js
@@ -0,0 +1,29 @@
+describe('Account', () => {
+
+  beforeEach(() => {
+    cy.exec('npm run seed')
+    cy.visit('/')
+  })
+
+  it('Login', () => {
+    cy.visit('/')
+    cy.contains('.title', 'Open Product Evaluation')
+
+    cy.login('jane@doe.com', 'password')
+
+    cy.get('.navbar .dropdown-toggle span')
+      .should('contain', 'Jane Doe')
+  })
+
+  it('Logout', () => {
+    cy.login('jane@doe.com', 'password')
+
+    cy.get('.b-nav-dropdown')
+      .click()
+
+    cy.get('.b-nav-dropdown .dropdown-menu a:last-child')
+      .click()
+
+    cy.contains('.title', 'Open Product Evaluation')
+  })
+})

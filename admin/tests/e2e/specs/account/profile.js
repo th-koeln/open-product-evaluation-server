1bb8c64c33f09ea64ce87b59fed52f73211b386d
Sascha Lemke <sascha-lemke@gmx.de>
2019-02-10 02:59:17 +0100

    added profile tests

---
+++ admin/tests/e2e/specs/account/profile.js
@@ -0,0 +1,35 @@
+describe('Profile', () => {
+
+  beforeEach(() => {
+    cy.exec('npm run seed')
+    Cypress.env('NODE_ENV')
+    cy.visit('/')
+    cy.login('jane@doe.com', 'password')
+  })
+
+  it('update profile', () => {
+
+    cy.get('.b-nav-dropdown')
+      .click()
+
+    cy.get('.b-nav-dropdown .dropdown-menu a:first-child')
+      .click()
+
+    cy.url()
+      .should('contain', '/#/profile')
+
+    cy.get('#input_firstname')
+      .clear()
+      .type('Jasonn')
+
+    cy.get('.btn-primary')
+      .click()
+
+    cy.visit('/#/survey')
+
+    cy.visit('/#/profile')
+      .get('#input_firstname')
+      .should('have.value', 'Jasonn')
+
+  })
+})

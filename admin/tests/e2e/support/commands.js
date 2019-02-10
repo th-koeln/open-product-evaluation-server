0cb1e049f7ad6181ce9350efef0268d87af041e6
Sascha Lemke <sascha-lemke@gmx.de>
2019-02-10 13:13:00 +0100

    moved visit to login command

--- admin/tests/e2e/support/commands.js
+++ admin/tests/e2e/support/commands.js
@@ -10,7 +10,10 @@
 //
 //
 // -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => { 
+Cypress.Commands.add('login', (email, password) => {
+
+  cy.visit('/')
+  
   cy.get('#input_email')
     .type(email)
     .get('#input_password')

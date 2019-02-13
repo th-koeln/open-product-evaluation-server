7b350b7fa5e33463f749edb53804d2d21cb7ad14
Sascha Lemke <sascha-lemke@gmx.de>
2019-02-14 00:41:55 +0100

    moved to one src folder

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
+const path = require('path')
+
 module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@':  path.resolve(__dirname, './src/views/'),
+      },
+    },
+    /*entry: {
+      app: [
+        './src/views/main.js'
+      ]
+    }*/
+  },
   css: {
     loaderOptions: {
       sass: {

c461445de1524398d0c136dab60a00e9df4b4f56
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-20 14:52:29 +0000

    Deploy th-koeln/open-product-evaluation-server to github.com/th-koeln/open-product-evaluation-server.git:gh-pages

--- src/entities/misc/misc.resolvers.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Dennis Dubbert on 28.06.18.
- */
-const { GraphQLDateTime } = require('graphql-iso-date')
-
-module.exports = {
-  DateTime: GraphQLDateTime,
-}
--- src/utils/roles.js
+++
@@ -1,5 +0,0 @@
-module.exports = Object.freeze({
-  ADMIN: 'ADMIN',
-  USER: 'USER',
-  DEVICE: 'DEVICE',
-})

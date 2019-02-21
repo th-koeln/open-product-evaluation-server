4fb1f269a4ae13726725938d6c7cacf13e42fad8
ddubbert <dennis.dubbert@smail.th-koeln.de>
2019-02-21 20:06:36 +0100

    adjusted versions graphql and mongoose schema to contain summaries

--- src/entities/version/version.schema.js
+++ src/entities/version/version.schema.js
@@ -3,6 +3,39 @@ const mongoose = require('mongoose')
 
 const { Schema } = mongoose
 
+const SummaryData = new Schema({
+  total: {
+    type: Number,
+    required: [true, 'needs to be a Number'],
+  },
+  label: {
+    type: String,
+    required: [true, 'needs to be a String'],
+  },
+}, { timestamps: { createdAt: 'creationDate', updatedAt: 'lastUpdate' } })
+
+const Summary = new Schema({
+  question: {
+    type: Schema.Types.ObjectId,
+    get: id => ((id) ? id.toString() : null),
+    required: [true, 'needs to be an ID'],
+  },
+  type: {
+    type: String,
+    enum: ['CHOICE', 'FAVORITE', 'LIKE', 'LIKEDISLIKE', 'RANKING', 'REGULATOR'],
+    required: [true, 'needs to be a String'],
+  },
+  value: String,
+  metric: {
+    type: String,
+    required: [true, 'needs to be a String'],
+  },
+  data: {
+    type: [SummaryData],
+    required: [true, 'needs to have a label and a total']
+  },
+}, { timestamps: { createdAt: 'creationDate', updatedAt: 'lastUpdate' } })
+
 const Version = new Schema({
   survey: {
     type: Schema.Types.ObjectId,
@@ -20,7 +53,8 @@ const Version = new Schema({
   versionNumber: {
     type: Number,
     required: [true, 'needs to be a Number']
-  }
+  },
+  summaries: [Summary],
 }, { timestamps: { createdAt: 'from' } })
 
 module.exports = Version

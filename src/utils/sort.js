cc8c38c4f7cc16f2073fbaa868c419ec6ad440e4
ddubbert <dennis.dubbert@smail.th-koeln.de>
2019-02-26 14:46:52 +0100

    added sort function to sort answers

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -10,7 +10,18 @@ const sortObjectsByIdArray = (arrayOfIds, arrayOfObjects) => {
   return _.sortBy(arrayOfObjects, object => sortObj[object.id])
 }
 
+const sortAnswersByQuestionIdArray = (arrayOfQuestionIds, arrayOfAnswers) => {
+  /** Convert array of ids to Object with id:index pairs* */
+  const sortObj = arrayOfQuestionIds.reduce((acc, id, index) => ({
+    ...acc,
+    [id]: index,
+  }), {})
+  /** Sort questions depending on the former Array of ids * */
+  return _.sortBy(arrayOfAnswers, answer => sortObj[answer.question])
+}
+
 module.exports = Object.freeze({
-  sortObjectsByIdArray
+  sortObjectsByIdArray,
+  sortAnswersByQuestionIdArray
 })
 

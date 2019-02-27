b16ca8bd7dea739a1fe53fc481653f770ab060ff
ddubbert <dennis.dubbert@smail.th-koeln.de>
2019-02-27 11:55:51 +0100

    created routes folder for image-download and csv-export routes

---
+++ src/routes/exports.js
@@ -0,0 +1,44 @@
+const archiver = require('archiver')
+
+const config = require('../../config')
+const csvCreator = require('../visualization/csv.creator')
+const { getMatchingId } = require('../store/id.store')
+
+module.exports = (models) => {
+  const getExport =  async (req, res) => {
+    const { surveyID, versionNumber } = req.params
+
+    try {
+      const [version] = await models.version.get({
+        versionNumber,
+        survey: getMatchingId(surveyID),
+      })
+
+      const csvData = await csvCreator.createCSVForVersion(version, models)
+
+      const archive = archiver('zip', {
+        zlib: { level: 9 },
+      })
+
+      res.attachment(`version${versionNumber}.zip`)
+
+      archive.pipe(res)
+
+      Object.keys(csvData).forEach((key) => {
+        archive.append(csvData[key], { name: `${key}.csv` })
+      })
+
+      archive.finalize()
+    } catch (e) {
+      res.status(404).send()
+    }
+  }
+
+  const addExportsRoute = (express) =>
+    express.use(`${config.app.exportRoute}/:surveyID/:versionNumber`, getExport)
+
+  return Object.freeze({
+    addExportsRoute
+  })
+}
+

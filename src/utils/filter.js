49c4b8a99e699ef44528794e65b24acea95cffcb
ddubbert <dennis.dubbert@smail.th-koeln.de>
2019-02-28 18:12:19 +0100

    created new filter methods for client, domain, survey, user and vote

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -11,47 +11,164 @@ const getPaginationLimitFromRequest = pagination => pagination ? pagination.limi
 // eslint-disable-next-line
 const getPaginationOffsetFromRequest = pagination => pagination ? pagination.offset : undefined
 
-// TODO: maybe regex and black-/whitelist characters
-const getQueryObjectForParameter = parameter => Object.keys(parameter).reduce((acc, key) => {
-  const { filterMethod } = parameter[key]
-  const paramObject = {
-    [key]: { [filterMethod]: parameter[key].value },
+const getStartsWithFilter = (attribute, startsWith) => ({
+  [attribute]: { $regex: `^${startsWith}`, $options: 'i' }
+})
+
+const createOwnerFilter = (name) => {
+  const firstNameFilter = getStartsWithFilter('firstName', name)
+  const lastNameFilter = getStartsWithFilter('lastName', name)
+  const emailFilter = getStartsWithFilter('email', name)
+
+  return [firstNameFilter, lastNameFilter, emailFilter]
+}
+
+const createSurveyFilter = async (filterBy, models) => {
+  if (!filterBy) return {}
+
+  let filter = {}
+
+  if (filterBy.title) filter = getStartsWithFilter('title', filterBy.title)
+
+  if (Object.prototype.hasOwnProperty.call(filterBy, 'isActive')) filter.isActive = filterBy.isActive
+
+  if (filterBy.creator) {
+    const ownerFilters = createOwnerFilter(filterBy.creator)
+
+    try {
+      const possibleCreatorIds = (await models.user.get({
+        $or: ownerFilters,
+      })).map(user => user.id)
+
+      filter.creator = { $in: possibleCreatorIds }
+    } catch (e) { throw new Error('No Survey found.') }
   }
 
-  if (filterMethod === '$regex') {
-    paramObject[key].$options = 'i'
+  return filter
+}
+
+
+const createUserFilter = async (filterBy) => {
+  if (!filterBy) return {}
+
+  let filter = {}
+
+  if (filterBy.firstName) filter = getStartsWithFilter('firstName', filterBy.firstName)
+
+  if (filterBy.lastName) filter.lastName = getStartsWithFilter('lastName', filterBy.lastName)
+
+  if (filterBy.email) filter.email = getStartsWithFilter('email', filterBy.email)
+
+  if (filterBy.isAdmin) filter.isAdmin = filterBy.isAdmin
+
+  return filter
+}
+
+const createClientFilter = async (filterBy, models) => {
+  if (!filterBy) return {}
+
+  let filter = {}
+
+  if (filterBy.name) filter = getStartsWithFilter('name', filterBy.name)
+
+  if (filterBy.domain) {
+    const domainNameFilter = getStartsWithFilter('name', filterBy.domain)
+
+    try {
+      const possibleDomainIds = (await models.domain.get(domainNameFilter)).map(d => d.id)
+
+      filter.domain = { $in: possibleDomainIds }
+    } catch (e) { throw new Error('No Client found.') }
   }
 
-  return { ...acc, ...paramObject }
-}, [])
+  if (filterBy.owner) {
+    const ownerFilters = createOwnerFilter(filterBy.owner)
 
-const getQueryObjectForFilter = (filter) => {
-  if (!filter) {
-    return {}
+    try {
+      const possibleOwnerIds = (await models.user.get({
+        $or: ownerFilters,
+      })).map(user => user.id)
+
+      filter.owners = { $in: possibleOwnerIds }
+    } catch (e) {
+      throw new Error('No Client found.')
+    }
   }
 
-  let childFilter = []
+  return filter
+}
 
-  if (filter.moreFilter) {
-    childFilter = [
-      ...childFilter,
-      ...filter.moreFilter.map(f => getQueryObjectForFilter(f)),
-    ]
+const createDomainFilter = async (filterBy, models) => {
+  if (!filterBy) return {}
+
+  let filter = {}
+
+  if (filterBy.name) filter = getStartsWithFilter('name', filterBy.name)
+
+  if (filterBy.activeSurvey) {
+    const surveyTitleFilter = getStartsWithFilter('title', filterBy.activeSurvey)
+
+    try {
+      const possibleSurveyIds = (await models.survey.get(surveyTitleFilter)).map(s => s.id)
+
+      filter.activeSurvey = { $in: possibleSurveyIds }
+    } catch (e) { throw new Error('No Domain found.') }
   }
 
-  if (filter.parameters) {
-    childFilter = [
-      ...childFilter,
-      ...filter.parameters.map(p => getQueryObjectForParameter(p)),
-    ]
+  if (Object.prototype.hasOwnProperty.call(filterBy, 'isPublic')) filter.isPublic = filterBy.isPublic
+
+  if (filterBy.owner) {
+    const ownerFilters = createOwnerFilter(filterBy.owner)
+
+    try {
+      const possibleOwnerIds = (await models.user.get({
+        $or: ownerFilters,
+      })).map(user => user.id)
+
+      filter.owners = { $in: possibleOwnerIds }
+    } catch (e) {
+      throw new Error('No Domain found.')
+    }
   }
 
-  return { [filter.connector]: childFilter }
+  return filter
+}
+
+const createVoteFilter = async (filterBy, models) => {
+  if (!filterBy) return {}
+
+  let filter = {}
+
+  if (filterBy.domain) {
+    const domainNameFilter = getStartsWithFilter('name', filterBy.domain)
+
+    try {
+      const possibleDomainIds = (await models.domain.get(domainNameFilter)).map(d => d.id)
+
+      filter.domain = { $in: possibleDomainIds }
+    } catch (e) { throw new Error('No Vote found.') }
+  }
+
+  if (filterBy.client) {
+    const clientNameFilter = getStartsWithFilter('name', filterBy.client)
+
+    try {
+      const possibleClientIds = (await models.client.get(clientNameFilter)).map(c => c.id)
+
+      filter.client = { $in: possibleClientIds }
+    } catch (e) { throw new Error('No Vote found.') }
+  }
+
+  return filter
 }
 
 module.exports = Object.freeze({
   getSortObjectFromRequest,
   getPaginationLimitFromRequest,
   getPaginationOffsetFromRequest,
-  getQueryObjectForFilter,
+  createSurveyFilter,
+  createUserFilter,
+  createClientFilter,
+  createDomainFilter,
+  createVoteFilter,
 })

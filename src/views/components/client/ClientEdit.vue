23df3f850641abfd8787b3dacf7eb24fbcf1ee55
Sascha Lemke <sascha-lemke@gmx.de>
2019-02-28 11:52:03 +0100

    added form validation to edit client form

--- src/views/components/client/ClientEdit.vue
+++ src/views/components/client/ClientEdit.vue
@@ -7,7 +7,14 @@
       <b-form-group label="Name"
                     label-for="input_name">
         <b-input id="input_name"
-                 v-model="client.name" />
+                 v-model.trim="$v.client.name.$model"
+                 :state="state($v.client.name.$dirty, $v.client.name.$error)" />
+        <b-form-invalid-feedback v-if="!$v.client.name.required">
+          Client name is required
+        </b-form-invalid-feedback>
+        <b-form-invalid-feedback v-if="!$v.client.name.alpha">
+          Client name only allows alphabet characters
+        </b-form-invalid-feedback>
       </b-form-group>
 
       <b-btn variant="primary"
@@ -20,39 +27,56 @@
 
 <script>
 import Alert from '@/components/misc/ErrorAlert.vue'
+import validationState from '@/mixins/validation'
+import {
+  required,
+  alpha,
+} from 'vuelidate/lib/validators'
 
 export default {
   name: 'ClientEdit',
   components: {
     alert: Alert,
   },
+  mixins: [validationState],
   data() {
     return {
       error: null,
+      client: {
+        name: '',
+      }
     }
   },
-  computed: {
-    client() {
-      return JSON.parse(JSON.stringify(this.$store.getters.getClient))
-    },
+  validations: {
+    client: {
+      name: {
+        required,
+        alpha,
+      },
+    }
   },
   created() {
     this.$store.dispatch('getClient', {
       id: this.$route.params.id,
+    }).then((data) => {
+      this.client = Object.assign({}, data.data.client)
     }).catch((error) => {
       this.error = error
     })
   },
   methods: {
     updateClient(event) {
-      event.preventDefault()
-      this.$store.dispatch('updateClient', this.client)
-        .then(() => {
-          this.$router.push('/clients')
-        })
-        .catch((error) => {
-          this.error = error
-        })
+      this.$v.$touch()
+
+      if (!this.$v.$invalid) {
+        this.$store.dispatch('updateClient', this.client)
+          .then(() => {
+            this.$router.push('/clients')
+          })
+          .catch((error) => {
+            this.error = error
+          })
+      }
     },
   },
 }

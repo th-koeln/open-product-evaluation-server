647e2be3e1123cd76adec3157465989d53ad2d1b
Sascha Lemke <sascha-lemke@gmx.de>
2019-03-05 00:32:49 +0100

    added confirm password field to profile form

--- src/views/components/account/ProfileForm.vue
+++ src/views/components/account/ProfileForm.vue
@@ -58,6 +58,18 @@
         </b-form-invalid-feedback>
       </b-form-group>
 
+      <b-form-group label="Repeat Password"
+                    label-for="input_password_repeat">
+        <b-form-input id="input_password_repeat"
+                      v-model.trim="$v.user.password_repeat.$model"
+                      type="password"
+                      :state="state($v.user.password_repeat.$dirty, $v.user.password_repeat.$error)" />
+
+        <b-form-invalid-feedback v-if="!$v.user.password_repeat.sameAs">
+          Your passwords don't match
+        </b-form-invalid-feedback>
+      </b-form-group>
+
       <b-btn type="submit"
              variant="primary">
         Update
@@ -75,6 +87,7 @@ import {
   email,
   alpha,
   minLength,
+  sameAs
 } from 'vuelidate/lib/validators'
 
 export default {
@@ -113,6 +126,9 @@ export default {
       password: {
         minLength: minLength(4),
       },
+      password_repeat: {
+        sameAs: sameAs('password'),
+      },
     }
   },
   created() {
@@ -121,6 +137,7 @@ export default {
     }).then((data) => {
       const user = Object.assign({}, data.data.user)
       user.password = ''
+      user.password_repeat = ''
       this.user = user
     }).catch((error) => {
       this.error = error

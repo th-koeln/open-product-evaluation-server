bb508b82b3da40bb3071bd78c19150e21dbdcad4
Sascha Lemke <sascha-lemke@gmx.de>
2019-03-17 11:20:50 +0100

    added client tests

---
+++ tests/e2e/specs/client/client.js
@@ -0,0 +1,27 @@
+describe('Client', () => {
+
+  beforeEach(() => {
+    cy.exec('npm run seed')
+    cy.login('jane@doe.com', 'password')
+  })
+
+  it('Edit client', () => {
+
+  })
+
+  it('Delete client', () => {
+
+  })
+
+  it('Add Owner to Client', () => {
+
+  })
+
+  it('Remove Owner from Client', () => {
+    
+  })
+
+  it('Search Clients', () => {
+    
+  })
+})

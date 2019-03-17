c4bce2ef07ac1d36d0ceb434a55f4929a9b4e192
Sascha Lemke <sascha-lemke@gmx.de>
2019-03-17 11:23:02 +0100

    added survey test

--- tests/e2e/specs/survey/survey.js
+++ tests/e2e/specs/survey/survey.js
@@ -1,28 +1,10 @@
 describe('Survey', () => {
 
   beforeEach(() => {
-    cy.exec('npm run seed')
-    cy.login('jane@doe.com', 'password')
+
   })
 
   it('Create Survey', () => {
 
-    cy.get('.btn-primary')
-      .click()
-
-    cy.url()
-      .should('contain', '/survey/new')
-
-    cy.get('#input_title')
-      .type('test survey')
-
-    cy.get('#input_description')
-      .type('Dies ist eine Test Survey!')
-
-    cy.get('.btn-primary')
-      .click()
-
-    cy.get('.container div')
-      .should('have.class', 'survey')
   })
 })

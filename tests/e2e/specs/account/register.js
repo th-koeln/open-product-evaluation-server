d7e461f5b84a6a016d014b2a900977403bd797ca
Sascha Lemke <sascha-lemke@gmx.de>
2019-03-17 22:27:44 +0100

    added register error tests

--- tests/e2e/specs/account/register.js
+++ tests/e2e/specs/account/register.js
@@ -6,6 +6,41 @@ describe('Register', () => {
     cy.visit('/#/register')
   })
 
+  it('Should require all fields', function() {
+    cy.get('.btn-primary')
+      .click()
+
+    cy.contains('.invalid-feedback', 'Firstname is required')
+    cy.contains('.invalid-feedback', 'Lastname is required')
+    cy.contains('.invalid-feedback', 'Email is required')
+    cy.contains('.invalid-feedback', 'Password is required')
+    cy.contains('.invalid-feedback', 'You need to repeat the password')
+  })
+
+  it('Should require a valid email', function() {
+    cy.get('#input_email')
+      .type('email')
+
+    cy.contains('.invalid-feedback', 'Email needs to be valid')
+  })
+
+  it('Should show error when password is to short', function() {
+    cy.get('#input_password')
+      .type('123')
+
+    cy.contains('.invalid-feedback', 'Password needs to be atleast 4 characters long')    
+  })
+
+  it('Should show error when password does not match', function() {
+    cy.get('#input_password')
+      .type('1234')
+
+    cy.get('#input_password_repeat')
+      .type('12345')
+
+    cy.contains('.invalid-feedback', 'Your passwords don\'t match')
+  })
+
   it('Should register new user', function() {
 
     cy.get('#input_firstname')

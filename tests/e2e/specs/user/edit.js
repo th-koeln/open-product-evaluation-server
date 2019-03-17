668cc8a46aadf6b57c6589c6ef996092da599e36
Sascha Lemke <sascha-lemke@gmx.de>
2019-03-17 11:22:02 +0100

    added user tests

---
+++ tests/e2e/specs/user/edit.js
@@ -0,0 +1,16 @@
+describe('User', () => {
+
+  beforeEach(() => {
+    cy.exec('npm run seed')
+    cy.login('jane@doe.com', 'password')
+  })
+
+  it('Edit User', () => {
+
+  })
+
+
+  it('Search User', () => {
+    
+  })
+})

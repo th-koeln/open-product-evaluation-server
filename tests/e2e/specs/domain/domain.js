ac35f45a0d362a6fc82172af51294da0148fc09c
Sascha Lemke <sascha-lemke@gmx.de>
2019-03-17 11:21:40 +0100

    added domain tests

---
+++ tests/e2e/specs/domain/domain.js
@@ -0,0 +1,39 @@
+describe('Domain', () => {
+
+  beforeEach(() => {
+    cy.exec('npm run seed')
+    cy.login('jane@doe.com', 'password')
+  })
+
+  it('Create Domain', () => {
+
+  })
+
+  it('Edit Domain', () => {
+
+  })
+
+  it('Delete Domain', () => {
+
+  })
+
+  it('Add Owner to Domain', () => {
+
+  })
+
+  it('Remove Owner from Domain', () => {
+
+  })
+
+  it('Add Client to Domain', () => {
+
+  })
+
+  it('Remove Client from Domain', () => {
+    
+  })
+
+  it('Search Domains', () => {
+    
+  })
+})

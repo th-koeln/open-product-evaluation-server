3146433c1fe50ce46b3090acc745ea9cf21991d8
Sascha Lemke <sascha-lemke@gmx.de>
2019-03-17 17:58:31 +0100

    added domain tests

--- tests/e2e/specs/domain/domain.js
+++ tests/e2e/specs/domain/domain.js
@@ -1,40 +1,95 @@
 describe('Domain', () => {
 
   beforeEach(() => {
-    cy.fixture('users/register').as('register')
+    cy.fixture('users/admin').as('admin')
+    cy.fixture('domain').as('domain')
     cy.exec('npm run seed')
   })
 
-  it('Create Domain', () => {
-    // cy.login(this.admin.email, this.admin.password)
+  it('Should create Domain', function() {
+    cy.login(this.admin.email, this.admin.password)
 
+    cy.visit('/#/domain')
+
+    cy.get('.list-options > div:first-child .btn-primary')
+      .click()
+
+    cy.get('#input_name')
+      .type('This is a new domain')
+
+    cy.get('.btn-primary')
+      .click()
+
+    cy.get('.domains__grid .card')
+      .its('length')
+      .should('be', 3)
   })
 
-  it('Edit Domain', () => {
+  it('Should edit Domain', function() {
+    cy.login(this.admin.email, this.admin.password)
 
+    cy.visit('/#/domain')
+
+    cy.get('.domains__grid > .card:first-child .card-links .btn-link:first-child')
+      .click()
+
+    cy.get('#input_name')
+      .should('not.have.value', '')
+      .clear()
+      .type('renamed domain')
+
+    cy.get('form > button.btn-primary')
+      .click()
+
+    cy.contains('.alert-success', 'Domain update successful')
+
+    cy.get('.domains__grid .card:first-child')
+      .contains('.card-title', 'renamed domain')
   })
 
-  it('Delete Domain', () => {
+  it('Should delete Domain', function() {
+    cy.login(this.admin.email, this.admin.password)
 
-  })
+    cy.visit('/#/domain')
 
-  it('Add Owner to Domain', () => {
-
-  })
-
-  it('Remove Owner from Domain', () => {
-
-  })
-
-  it('Add Client to Domain', () => {
-
-  })
-
-  it('Remove Client from Domain', () => {
+    cy.get('.domains__grid > .card:first-child .card-links > button')
+      .click()
     
+    cy.get(`#modal-grid-${this.domain[1].id} .modal-content`)
+      .should('be.visible')
+      .get(`#modal-grid-${this.domain[1].id} .btn-primary`)
+      .click()
+      .get('.domains__grid')
+      .its('length')
+      .should('be', 1)
   })
 
-  it('Search Domains', () => {
-    
+  it('Should search Domains', function() {
+    cy.login(this.admin.email, this.admin.password)
+
+    cy.visit('/#/domain')
+
+    cy.get('.search-form input.sbx-custom__input')
+      .clear()
+      .type(this.domain[0].name)
+
+    cy.get('.domains__grid .card')
+      .its('length')
+      .should('be', 1)
+
+    cy.get('.search-form input.sbx-custom__input')
+      .clear()
+      .type(this.domain[0].name.substring(0, 4))
+
+    cy.get('.domains__grid .card')
+      .its('length')
+      .should('be', 1)
+
+    cy.get('.search-form input.sbx-custom__input')
+      .clear()
+
+    cy.get('.domains__grid .card')
+      .its('length')
+      .should('be', 2)
   })
 })
